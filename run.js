ab80d8537e0e39ea2a48d57b92c40a3b74248f9d
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-07-07 11:07:53 -0700

    fix: use correct environment variable

--- run.js
+++ run.js
@@ -4,6 +4,6 @@ import core from "@actions/core";
 import { main } from "./main.js";
 
 /** @type {import("@octokit/webhooks-types").ReleaseCreatedEvent} */
-const event = JSON.parse(readFileSync(process.env.GITHUB_EVENT, "utf8"));
+const event = JSON.parse(readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"));
 
 main(core, event);
